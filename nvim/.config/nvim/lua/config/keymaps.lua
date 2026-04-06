1c4a22ebf4abc7e448079127d02323e98376b4fc
mz-mittsuu <mizutani.mitsunobu.1022@gmail.com>
2026-04-06 16:20:49 +0900

    feat: improve neovim line movement keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,24 +10,26 @@
 local keymap = vim.keymap.set
 
 -- --------------------------------------------------
--- search
--- --------------------------------------------------
-keymap("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
-
--- --------------------------------------------------
 -- insert mode
 -- --------------------------------------------------
 keymap("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
 keymap("i", "kj", "<Esc>", { desc = "Exit insert mode with kj" })
 
 -- --------------------------------------------------
--- wrapped line movement
--- --------------------------------------------------
-keymap("n", "j", "gj", { noremap = true, silent = true, desc = "Down visual line" })
-keymap("n", "k", "gk", { noremap = true, silent = true, desc = "Up visual line" })
-
--- --------------------------------------------------
 -- line movement
 -- --------------------------------------------------
-keymap({ "n", "x" }, "H", "g0", { noremap = true, silent = true, desc = "Start of visual line" })
-keymap({ "n", "x" }, "L", "g$", { noremap = true, silent = true, desc = "End of visual line" })
+keymap({ "n", "x" }, "H", "g0", { remap = true, silent = true, desc = "Start of visual line" })
+keymap({ "n", "x" }, "L", "g$", { remap = true, silent = true, desc = "End of visual line" })
+
+keymap("n", "j", function()
+    return vim.v.count == 0 and "gj" or "j"
+end, { expr = true, remap = true, silent = true, desc = "Down visual line" })
+
+keymap("n", "k", function()
+    return vim.v.count == 0 and "gk" or "k"
+end, { expr = true, remap = true, silent = true, desc = "Up visual line" })
+
+-- --------------------------------------------------
+-- search
+-- --------------------------------------------------
+keymap("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
